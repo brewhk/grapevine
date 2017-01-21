7faea977cbb54ce1cabd392dc767f29bfd18d43a
Daniel Li <dan@danyll.com>
2017-01-21 17:55:55 +0000

    Updated webpack.config.js to use Webpack 2 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,18 @@ module.exports = {
     filename: './dist/bundle.js',
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
-        query: {
-          "presets": ["es2015", "react"]
-        }
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              "presets": ["es2015", "react"]
+            }
+          }
+        ]
       }
     ]
   },
